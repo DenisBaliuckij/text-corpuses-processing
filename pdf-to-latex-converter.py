435599dfd134ca07cb6fcd11fa2f41bb1d9d47de
Denis Baliuckij <denis.baliuckij@gmail.com>
2026-06-02 06:26:36 +0200

    refactor: simplify standalone converter script to delegate to pdfConverter module

--- pdf-to-latex-converter.py
+++ pdf-to-latex-converter.py
@@ -1,39 +1,9 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Fri Apr 10 13:08:42 2026
+import sys
+import os
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'dags'))
 
-@author: denis
-"""
+import pdfConverter
 
-# pip install pypdf
-from pypdf import PdfReader
-import ftpConnector
-from ftpConnector import ftpConnector
-import dbConnector
-from dbConnector import databaseConnector
-import io
-
-i = 0
-while True:   
-    i+=1
-    urlToConvert = databaseConnector.getPdfToConvertToLatex()
-    try:
-        file = ftpConnector.getFile(urlToConvert)
-        reader = PdfReader(file)
-        text = ""
-        for page in reader.pages:
-             text += page.extract_text()
-        result = io.StringIO(text)
-        filename = urlToConvert.replace('.pdf', '.tex')
-        ftpConnector.storeFile(filename, io.BytesIO(result.read().encode('utf8')), 'Tex')
-        print(urlToConvert)
-        print(filename)
-        databaseConnector.saveLatexFileLocation(urlToConvert, filename)
-        if i>500:
-            break
-    except Exception as e:
-        databaseConnector.saveLatexFileLocation(urlToConvert, 'NA')
-        
-
-
-
+if __name__ == '__main__':
+    converted = pdfConverter.run_conversion()
+    print(f"Converted {converted} files")
